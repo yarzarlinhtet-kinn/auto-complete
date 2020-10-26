43a29087100d8e8e31665ffa92d56e63f0730c08
Yar Zar Lin Htet <yarzar@management-partners.co.jp>
2020-10-26 11:17:30 +0630

    upgrade bootstrap 4

--- src/components/Autocomplete.vue
+++ src/components/Autocomplete.vue
@@ -1,20 +1,22 @@
 <template>
-<div style="position:relative" v-bind:class="{'open':openSuggestion}">
+<div style="position:relative" >
     <input class="form-control" type="text" v-model="selection"
         @keydown.enter = 'enter'
         @keydown.down = 'down'
         @keydown.up = 'up'
         @input = 'change'
     />
-    <ul class="dropdown-menu" style="width:100%">
-        <li v-for="(suggestion, index) in matches"
+    <div class="dropdown-menu" style="width:100%;" v-bind:class="{'show':openSuggestion}">
+        <a v-for="(suggestion, index) in matches"
             v-bind:class="{'active': isActive(index)}"
             @click="suggestionClick(index)"
             :key="index"
+            class="dropdown-item"
+            href='#'
         >
-            <a href="#">{{ suggestion.name }}</a>
-        </li>
-    </ul>
+            {{ suggestion.name }}
+        </a>
+    </div>
 </div>
 </template>
 <script>
@@ -89,4 +91,3 @@ export default {
 }
 
 </script>
-
